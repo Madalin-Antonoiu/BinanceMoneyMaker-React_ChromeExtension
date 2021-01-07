7f1411493176370e9f9a3800383d15a183c9694f
Madalin Antonoiu <antonoiumadalin@gmail.com>
2021-01-07 18:26:26 +0000

    Getting list with all coins

--- src/content-script/App.js
+++ src/content-script/App.js
@@ -1,11 +1,22 @@
-import React, { useEffect } from "react";
-import QueryData from "../react-components/QueryData";
+import React, { useEffect, useState } from "react";
+import binance from "../react-components/api/binance";
 
 const App = () => {
+  const [cryptocoins, setCryptocoins] = useState([]);
+
+  useEffect(() => {
+    async function search() {
+      const response = await binance.get("/exchangeInfo"); // means i get the data property from the response
+      setCryptocoins(response.data.symbols);
+    }
+    search();
+  }, []);
+
   return (
     <div>
-      <QueryData />
-      <button className="ui primary button">AWESOMENESS</button>;
+      {cryptocoins.map((cryptocoin) => (
+        <div key={cryptocoin.symbol}>{cryptocoin.symbol}</div>
+      ))}
     </div>
   );
 };
